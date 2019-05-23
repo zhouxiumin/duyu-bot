72e05b9b20baf46de99751835aa5d4de8d58c269
zhouxiumin <zhouxiuminmeiwen@gmail.com>
2019-05-23 12:01:05 +0800

    first import

---
+++ test.js
@@ -0,0 +1,34 @@
+const douyu_bot = require('./index');
+const roomid = '78561';
+const client = new douyu_bot(roomid);
+
+client.on('connect', () => {
+    console.log(`已连接douyu ${roomid}房间弹幕~`)
+});
+
+client.on('message', msg => {
+    switch (msg.type) {
+        case 'chat':
+            console.log(`[${msg.from.name}]:${msg.content}`);
+            break;
+        case 'gift':
+            console.log(`[${msg.from.name}]->赠送${msg.count}个${msg.name}`);
+            break;
+        case 'yuwan':
+            console.log(`[${msg.from.name}]->赠送${msg.count}个${msg.name}`);
+            break;
+        case 'deserve':
+            console.log(`[${msg.from.name}]->赠送${msg.count}个${msg.name}`);
+            break
+    }
+});
+
+client.on('error', e => {
+    console.log(e)
+});
+
+client.on('close', () => {
+    console.log('close')
+});
+
+client.start();
